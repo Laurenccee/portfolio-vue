d44dda1494e60f6d50cfe2ac83a4b2a3aa6de0aa
tejadarevertlaurence <tejadarevertlaurence@gmail.com>
2025-06-21 14:07:22 +0000

    starting

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-1 flex-col h-screen justify-center items-center">
+      <h1 class="xl:text-2xl lg:text-xl text-black uppercase">{ LAURENCE }</h1>
+      <p class="xl: text-xs">{ Aspiring Full-Satck Web Developer and UI/UX Designer. }</p>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #10B981;
+  }
+  </style>
+  
